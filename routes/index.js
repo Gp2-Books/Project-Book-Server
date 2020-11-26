d1c58c02ba0c27950bcdfc8119e83b5810d40120
robbykansas <lomiver@gmail.com>
2020-11-27 01:44:27 +0700

    1. add 3rd party API from goodreads to get rating 2. add API from openlibrary to get full title 3. add API from itBook to get desc etc 4. get all of them into 1 obj with chaining promise

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
-const { UserController } = require('../controllers')
+const { UserController, BookController } = require('../controllers')
 const authentication = require('../middlewares/authentication')
 
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 // router.get('/sample', authentication, UserController.sample)
+router.use(authentication)
+router.get('/book', BookController.book)
 
 module.exports = router
