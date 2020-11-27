5fa016e3a226c2c3a4189c8918836eaf17119e54
ogyrahmawan <ogyrahmawan@gmail.com>
2020-11-27 08:30:18 +0700

    Menambahkan fitur Google Login - menambahkan route /googleLogin - menambahkan controller googleLogin - install cors - install googleapis - update env

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const port = 3000;
 const errorHandler = require('./middlewares/errorHandler')
 const router = require('./routes');
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/', router);
